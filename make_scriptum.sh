e970a28fc6ca6e4c196f7ce1df91145b4e5e52a5
authmillenon <authmill@datalove.me>
2012-08-18 04:30:11 +0200

    Remove FUD

--- make_scriptum.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cat git_talk_script_frame.html |\
-    sed "s/{{ content }}/$(markdown_py -o html5 git_talk_script.md | 
-        sed -e 's/\//\\\//g' -e 's/$/\\n/g' | tr -d "\n")/ " \
-    > git_talk_script.html
